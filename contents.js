e002bae1da9848479551a65b259c43cdd72809bb
Georgia Hunt <georgialhunt89@gmail.com>
2021-05-21 01:32:26 +0930

    Note Taker README

--- contents.js
+++ contents.js
@@ -1,13 +1,13 @@
 const tableOfContents =[
     `
-'*[Description](#description)',
-'​*[Installation Instructions](#installation)',
-'*​[Usage Information](#usage)',
-'*[Contributors](#contributors)',
-'​*[Test Instructions](#testing)',
-'*[Deployment Link](#deployment)'
-'*[Questions](#questions)',
-'*​[License](#license)',
+*[Description](#description),
+​*[Installation Instructions](#installation),
+*​[Usage Information](#usage),
+*[Contributors](#contributors),
+​*[Test Instructions](#testing),
+*[Deployment Link](#deployment),
+*[Questions](#questions),
+*​[License](#license),
 `
 ];
 
